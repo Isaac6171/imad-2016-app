f1868759e4f061236bae60bc0eb464c5985d801d
Isaac6171 <isaac4700@gmail.com>
2016-10-26 19:00:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ app.get('/bio.html',function(req, res){
 app.get('/education.html',function(req, res){
     res.sendFile(path.join(__dirname,'ui', 'education.html'));
 });
+app.get('/resume.html',function(req, res){
+    res.sendFile(path.join(__dirname,'ui', 'resume.html'));
+});
+app.get('/contact.html',function(req, res){
+    res.sendFile(path.join(__dirname,'ui', 'contact.html'));
+});
 
 
 
@@ -49,6 +55,11 @@ app.get('/ui/IMG_20161010_202803.jpg', function (req, res) {
 app.get('/ui/Portfolio.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Portfolio.jpg'));
 });
+app.get('/ui/C.V-01.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'C.V-01.png'));
+});
+
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
