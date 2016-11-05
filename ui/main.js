fa383e745dd21547728ea555f61c4fab663f7007
Isaac6171 <isaac4700@gmail.com>
2016-11-05 19:42:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++
@@ -1,9 +0,0 @@
-var button = document.getElementById('counter');
-var counter = 0;
-button.onclick = function(){
- 
- 
- counter = counter+1;
- var th = document.getElementById('count');
- th.innerHTML = counter.toString();
-};
