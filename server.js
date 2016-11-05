768274363732d5f4666eff2d8576e40519c3ec7e
Isaac6171 <isaac4700@gmail.com>
2016-11-05 21:01:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -159,7 +159,12 @@ app.get('/:pageName',function(req, res){
     res.send(createTemplate(pages[pageName]));
 });
 
-
+var counter = 0;
+app.get('/counter',function(req,res){
+	counter = counter+1;
+	res.send(counter.toString());
+	
+	});
 
 
 
