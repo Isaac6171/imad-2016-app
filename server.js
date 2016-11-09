4fb60cabb1556803b2135c42c741a381aaf884ec
Isaac6171 <isaac4700@gmail.com>
2016-11-09 22:29:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+  
+  user: 'isaac6171',
+  database: 'isaac6171',
+  host: 'db.imad.hasura-app.io',
+  port:'5432',
+  password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -166,6 +176,10 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/test-db', function(req,res){
+    
 });
 
 var comments = [];
